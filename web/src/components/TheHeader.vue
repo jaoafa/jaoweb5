a8abe2bf545bfd54f4167840ba50400090d4255a
Hiratake <hiratake@hiratake.xyz>
2023-08-07 23:32:51 +0900

    feat: 開閉ナビゲーションのオーバーレイ部分を追加

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -33,6 +33,34 @@ const isTop = computed<boolean>(() => route.path === '/')
         <AppLogo />
       </NuxtLink>
 
+      <div
+        :class="[
+          'fixed left-0 top-0 h-[100dvh] w-[100dvw] overflow-hidden',
+          open ? 'pointer-events-auto' : 'pointer-events-none',
+        ]"
+      >
+        <div
+          :class="[
+            'circle',
+            'absolute z-0 rounded-full bg-gray-900',
+            'transition-transform duration-300 ease-out',
+            open ? 'scale-100' : 'scale-0',
+          ]"
+        />
+        <div
+          :class="[
+            'relative z-10',
+            'h-full overflow-y-auto overscroll-contain text-white',
+            'transition-opacity duration-100',
+            open ? 'opacity-100' : 'opacity-0',
+          ]"
+        >
+          <div class="m-auto box-content max-w-6xl px-6 py-20 md:py-32">
+            aaa
+          </div>
+        </div>
+      </div>
+
       <button
         :class="[
           'relative aspect-square w-9 overflow-hidden rounded-full md:w-12',
@@ -57,3 +85,14 @@ const isTop = computed<boolean>(() => route.path === '/')
     </div>
   </header>
 </template>
+
+<style lang="scss" scoped>
+.circle {
+  $diameter: calc(max(100dvw, 100dvh) * 2);
+
+  top: calc((100dvh - $diameter) / 2);
+  left: calc((100dvw - $diameter) / 2);
+  width: $diameter;
+  height: $diameter;
+}
+</style>
