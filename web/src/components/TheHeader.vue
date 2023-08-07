b858442dfa8d4ca1c60770785f68301a403dfbe3
Hiratake <hiratake@hiratake.xyz>
2023-08-07 23:20:55 +0900

    feat: ナビゲーション開閉用のボタンを追加

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -3,6 +3,8 @@ const { y: scrollY } = useWindowScroll()
 const config = useRuntimeConfig()
 const route = useRoute()
 
+const [open, toggle] = useToggle(false)
+
 /** トップページであるか */
 const isTop = computed<boolean>(() => route.path === '/')
 </script>
@@ -30,6 +32,28 @@ const isTop = computed<boolean>(() => route.path === '/')
       >
         <AppLogo />
       </NuxtLink>
+
+      <button
+        :class="[
+          'relative aspect-square w-9 overflow-hidden rounded-full md:w-12',
+          'hover:text-white',
+          'before:absolute before:inset-0 before:m-auto',
+          'before:rounded-full before:bg-gray-900',
+          'before:scale-0 before:transition-transform before:ease-out',
+          'hover:before:scale-100',
+          '[&>span]:absolute [&>span]:inset-x-0 [&>span]:m-auto',
+          '[&>span]:h-0.5 [&>span]:bg-current',
+          '[&>span]:duration-300 [&>span]:ease-in-out',
+          open ? 'text-white before:scale-100' : '',
+        ]"
+        aria-label="toggle navigation"
+        @click="() => toggle()"
+      >
+        <span :class="`${open ? 'top-1/2 w-0' : 'top-[35%] w-1/2'}`" />
+        <span :class="`top-1/2 w-1/2 ${open ? 'rotate-45' : ''}`" />
+        <span :class="`top-1/2 w-1/2 ${open ? '-rotate-45' : ''}`" />
+        <span :class="`${open ? 'top-1/2 w-0' : 'top-[65%] w-1/2'}`" />
+      </button>
     </div>
   </header>
 </template>
