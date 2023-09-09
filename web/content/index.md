eac0d736af3fd1c287c96022edc28bcbc70855d0
Hiratake <hiratake@hiratake.xyz>
2023-09-09 23:20:49 +0900

    feat: ギャラリーセクションを追加

--- web/content/index.md
+++ web/content/index.md
@@ -15,6 +15,16 @@ jao Community Server は、Discord を中心に活動をしているコミュニ
 このコミュニティにはやべーやつらが生息し、独特で最高にイカしたコミュニティをつくっています。
 ::
 
-:top-gallery-section
+::top-gallery-section
+---
+images:
+  - src: /images/main01
+    alt: 画像 01
+  - src: /images/main02
+    alt: 画像 02
+  - src: /images/main03
+    alt: 画像 03
+---
+::
 
 :top-blog-section
--- web/src/components/content/TopGallerySection.vue
+++ web/src/components/content/TopGallerySection.vue
@@ -1,7 +1,50 @@
+<script lang="ts" setup>
+type TopGalleryImage = {
+  src: string
+  alt: string
+}
+type TopGallerySectionProps = {
+  /** 一覧に表示する画像 */
+  images: TopGalleryImage[]
+}
+
+const props = withDefaults(defineProps<TopGallerySectionProps>(), {
+  images: () => [],
+})
+
+const generatedImages = computed(() =>
+  props.images.map((item, index) => ({
+    id: index,
+    src: item.src,
+    alt: item.alt,
+  })),
+)
+</script>
+
 <template>
   <section class="relative">
     <div class="absolute z-0 h-full w-full -skew-y-6 bg-gray-800" />
 
-    <div class="m-auto box-content max-w-6xl px-4 py-24 md:px-6"></div>
+    <div class="relative z-10 m-auto box-content max-w-6xl px-4 py-28 md:px-6">
+      <div class="grid grid-cols-fill-72 gap-8">
+        <div
+          v-for="image in generatedImages"
+          :key="image.id"
+          class="aspect-video w-full overflow-hidden rounded-lg"
+        >
+          <img
+            :src="`${image.src}/1920w.webp`"
+            :srcset="`
+              ${image.src}/856w.webp 856w
+              ${image.src}/1200w.webp 1200w
+              ${image.src}/1620w.webp 1620w
+              ${image.src}/1920w.webp 1920w
+            `"
+            :alt="image.alt"
+            class="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
